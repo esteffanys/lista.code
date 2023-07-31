5851c25a6abcbe2adae2797e10cc5dc4a20a04eb
esteffanys <silmara.e@escolar.ifrn.edu.br>
2023-07-31 18:30:38 -0300

    SUBINDO LISTA

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+// importando rota para atroca de paginas
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(private router: Router) {}
+  ir2() {
+    this.router.navigateByUrl('/tabs/tab2');
+  }
+ ir3() {
+    this.router.navigateByUrl('/tabs/tab3');
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { ToastController } from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+
+
 export class Tab2Page {
-
+  // shoppingItems: { name: string; price: number; quantity: number }[] = [];
+  // total: number = 0;
+  // newItemName: string = '';
+  shoppingItems: { name: string; price: number; quantity: number }[] = [];
+  total: number = 0;
+  newItemName: string = '';
+  totalLimit: number = 0;
   constructor() {}
+//   updateTotal() {
+//     this.total = this.shoppingItems
+//       .map((item) => item.price * item.quantity)
+//       .reduce((acc, curr) => acc + curr, 0);
+//   }
 
+//   addItem() {
+//     if (this.newItemName && this.newItemName.trim() !== '') {
+//       this.shoppingItems.push({ name: this.newItemName.trim(), price: 0, quantity: 0 });
+//       this.newItemName = '';
+//       this.updateTotal();
+//     }
+//   }
+updateTotal() {
+  this.total = this.shoppingItems
+    .map((item) => item.price * item.quantity)
+    .reduce((acc, curr) => acc + curr, 0);
 }
+
+addItem() {
+  if (this.newItemName && this.newItemName.trim() !== '') {
+    this.shoppingItems.push({ name: this.newItemName.trim(), price: 0, quantity: 0 });
+    this.newItemName = '';
+    this.updateTotal();
+  }
+}
+removeItem(index: number) {
+  this.shoppingItems.splice(index, 1);
+  this.updateTotal();
+}
+
+clearList() {
+  this.shoppingItems = [];
+  this.total = 0;
+  this.newItemName = '';
+  this.totalLimit = 0;
+}
+}
+
