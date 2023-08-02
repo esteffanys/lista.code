0de2a0adc3673bbaac4c5eee51fb37b1f999c4d1
silmara esteffany matias freire <20211148060020@ifrn.local>
2023-08-02 12:45:20 -0300

    ultimo 1.1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-
+import tt  from '@tomtom-international/web-sdk-maps';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  map: any;
+  lat: number = -5.9381308
+  lon: number = -35.2682346
+  apikey: string = "1PDOTCf2WtVBoHBYXUMHHgY1qfxNjex4"
+  center: any = new tt.LngLat(this.lon, this.lat)
+  constructor( public Http: HttpClient) {
+    this.obterlocalizacao
+  }
+  ngAfterViewInit(){
+    this.map = tt.map({
+      key: this.apikey, //TomTom, not Google Maps
+      container: "map",
+      center: new tt.LngLat(this.lon, this.lat),
+      zoom: 14,
+      });
+      this.search("supermecado")
+      
+  }
 
-  constructor() {}
+  async obterlocalizacao(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.lat = coordinates.coords.latitude;
+    this.lon = coordinates.coords.longitude
+    console.log(this.lon, this.lat)
+  }
+  async search(poi: string){
+    this.obterlocalizacao();
+    const res: any = 
+    await this.Http.get(
+      `https://api.tomtom.com/search/2/search/&{poi}.json?lat=${this.lat}&lon=${this.lon}&key=${this.apikey}`).toPromise();
+      console.log(res.results);
 
+      let marker = new tt.Marker().setLngLat(this.center).addTo(this.map);
+      
+  }
 }
